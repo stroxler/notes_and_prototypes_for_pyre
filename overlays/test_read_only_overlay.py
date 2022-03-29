54c4273a7f5ac5c702c64110ac31f67c1bf76121
Steven Troxler <steven.troxler@gmail.com>
2022-03-29 09:15:11 -0700

    Add read-only-based overlay
    This change captures the essence of my plan for how to refactor
    the ocaml env stack and allow overlays:
    - All read-only data access happens via function calls (that change
      was made in `read_only`
    - As a result, we're able to implement `overlay` by wrapping function
      calls to the parent environment with logic to "intercept" lookups
      that are in the overlay scope.
    
    This implementation uses no caching in the overlay, but it's hopefully
    clear that inside the definition of `get` we could close over a dict
    and use that as a cache if we wanted; this is very similar to the
    ocaml situation where our overlay can close over the definition of
    some cache table if we need.
    
    Note that I don't pass dependencies in `get` functions. That's an
    optimization that is mostly specific to the python implementation,
    since we don't handle failed lookups in this toy. The real ocaml
    code is robust to a key not existing (everythign returns optional
    values) so it shouldn't matter much whether or not we include
    dependencies, we can just do whichever is easiest given the actual
    code structure.

---
+++ overlays/test_read_only_overlay.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+import functools
+from read_only_overlay import create_env_stack
+
+
+def debug(f):
+    @functools.wraps(f)
+    def wrapper():
+        try:
+            f()
+        except Exception:
+            import pdb;
+            import traceback
+            import sys
+
+            traceback.print_exc()
+            _, _, tb = sys.exc_info()
+            pdb.post_mortem(tb)
+
+    return wrapper
+
+
+
+@debug
+def test_env_stack():
+    (
+        code_env,
+        ast_env,
+        class_body_env,
+        class_parents_env,
+        class_grandparents_env
+    ) = create_env_stack(code={
+        "a": """
+            class X: pass
+            class Y(a.X): pass
+        """,
+        "b": """
+            class Z(a.X): pass
+            class W(b.Z): pass
+        """,
+    })
+    assert class_grandparents_env.get("b.Z", "") == []
+    assert class_grandparents_env.get("b.W", "") == ["a.X"]
+
+    class_grandparents_env.update("b", code="""
+        class Z(a.Y): pass
+        class W(b.Z): pass
+    """)
+    assert class_grandparents_env.get("b.Z", "") == ["a.X"]
+    assert class_grandparents_env.get("b.W", "") == ["a.Y"]
+
+
+
+@debug
+def test_overlay():
+    (
+        code_env,
+        ast_env,
+        class_body_env,
+        class_parents_env,
+        class_grandparents_env
+    ) = create_env_stack(code={
+        "a": """
+            class X: pass
+            class Y(a.X): pass
+        """,
+        "b": """
+            class Z(a.X): pass
+            class W(b.Z): pass
+        """,
+    })
+    assert class_grandparents_env.get("b.Z", "") == []
+    assert class_grandparents_env.get("b.W", "") == ["a.X"]
+
+    class_grandparents_overlay_get = class_grandparents_env.overlay(
+        module="b",
+        code="""
+            class Z(a.Y): pass
+            class W(b.Z): pass
+        """,
+    )
+    assert class_grandparents_overlay_get("b.Z", "") == ["a.X"]
+    assert class_grandparents_overlay_get("b.W", "") == ["a.Y"]
+
+    # the original env should still work
+    assert class_grandparents_env.get("b.Z", "") == []
+    assert class_grandparents_env.get("b.W", "") == ["a.X"]
